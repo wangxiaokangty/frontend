2f26100d532f22baf4cac3e82b6fec839a76fb1c
wangxiaokangty <1002075358@qq.com>
2024-01-15 22:24:28 +0800

    添加了 mavon-editor，用来实现 markdown 的编写

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
 
 
 
@@ -12,5 +15,6 @@ createApp(App)
     .use(ElementPlus)
     .use(store)
     .use(router)
+    .use(mavonEditor)
     .mount('#app')
 
