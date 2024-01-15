76f4f7e45a06af08fbf1c698a0d3e14b3b77e635
wangxiaokangty <1002075358@qq.com>
2024-01-16 00:04:10 +0800

    把 axios 作为了全局属性，不必每次都导入

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,10 +28,9 @@ export default {
   props: {
     msg: String
   },
-
   methods: {
     fetchdata() {
-      axios.get("http://localhost:8080/findById?id=1").then(res => {
+      this.$axios.get("http://localhost:8080/findById?id=1").then(res => {
         alert(res.data.id),
             this.message = res.data.id
       })
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,15 +6,19 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
+import axios from "axios";
 
 
+const app = createApp(App)
 
 
+app.config.globalProperties.$axios = axios;
 
-createApp(App)
+app
     .use(ElementPlus)
     .use(store)
     .use(router)
     .use(mavonEditor)
     .mount('#app')
+
 
